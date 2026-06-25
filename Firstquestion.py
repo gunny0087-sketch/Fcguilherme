2b3238f281d2e387a2192e0b271f2404e5769a32
gunny0087-sketch <gunny0087@gmail.com>
2026-06-24 22:31:24 -0300

    Update Firstquestion.py

--- Firstquestion.py
+++ Firstquestion.py
@@ -1,1 +1,8 @@
 
+sobrenome = input("Informe o sobrenome do apresentador: ")
+if sobrenome == "Pinheiro" or sobrenome == "Araújo":
+ print("Bom Dia Nação")
+elif sobrenome == "Bonner" or sobrenome == "Vasconcelos":
+    print("Jornal Brasileiro")
+else:
+    print("Apresentador(a) Desconhecido(a).")
