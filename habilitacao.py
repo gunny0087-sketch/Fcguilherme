3d8a4bacdb7a72f63c1d934491fa87006fb59003
gunny0087-sketch <gunny0087@gmail.com>
2026-06-24 22:29:02 -0300

    Update habilitacao.py

--- habilitacao.py
+++ habilitacao.py
@@ -1,1 +1,6 @@
-
+idade = int(input("Digite sua idade: "))
+habilitacao = input("Você tem habilitação?: ")
+if idade >= 18 and habilitacao == "sim":
+    print("Pode dirigir")
+else:
+    print("Não pode dirigir")
