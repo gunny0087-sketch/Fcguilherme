73d115fd60a6a3ee4b14fad75ec32b172fdc180a
gunny0087-sketch <gunny0087@gmail.com>
2026-06-24 22:27:10 -0300

    Update Questao.py

--- Questao.py
+++ Questao.py
@@ -1,1 +1,13 @@
 
+estatura1 = float(input("Digite  a primeira estatura: "))
+estatura2 = float(input("Digite a segunda estatura: "))
+estatura3 = float(input("Digite a terceira estatura: "))
+if  estatura1 == estatura2 or estatura1 == estatura3 or estatura2 == estatura3:
+    print("Há, pelo menos, 2 pessoas com a mesma estatura")
+else:
+  if estatura1 > estatura2 and estatura1 > estatura3:
+        print("A maior estatura é:", estatura1)
+  elif estatura2 > estatura1 and estatura2 > estatura3:
+        print("A maior estatura é:", estatura2)
+  else:
+        print("A maior estatura é:", estatura3)
