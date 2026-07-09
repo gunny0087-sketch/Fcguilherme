1daa8b7cbaf4b0fb0be67bb8a90758a2c67acae2
gunny0087-sketch <gunny0087@gmail.com>
2026-07-08 21:08:00 -0300

    Update questao3.py

--- questao3.py
+++ questao3.py
@@ -1,1 +1,14 @@
+chances= 0
+while chances< 3:
+    usuario = input("Usuário: ")
+    senha = input("Senha: ")
+    if usuario == "aluno" and senha == "12345":
+        print("Acesso liberado")
+        break
+    else:
+        chances += 1
 
+        if chances== 3:
+            print("Você teve 3 tentativas")
+        else:
+            print("Tente novamente")
