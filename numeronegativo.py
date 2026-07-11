bf960119747af0d7d9a576b006b492c57a60833e
gunny0087-sketch <gunny0087@gmail.com>
2026-07-10 23:01:00 -0300

    Update numeronegativo.py

--- numeronegativo.py
+++ numeronegativo.py
@@ -1,1 +1,20 @@
-
+soma = 0
+numeropositivo = 0
+while True:
+    numero = int(input("Digite um número: "))
+    if numero < 0:
+        break
+    soma += numero
+    if numeropositivo == 0:
+        maior = numero
+    else:
+        if numero > maior:
+            maior = numero
+    numeropositivo += 1
+if numeropositivo > 0:
+    media = soma / numeropositivo
+    print(f"A soma de todos os números é {soma}")
+    print(f"A média aritmética dos números é {media}")
+    print(f"O maior número é {maior}")
+else:
+    print("Nenhum número positivo foi digitado.")
