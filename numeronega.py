3605e93bd30f3b29ff0d276e09ec730a2f32f3fa
gunny0087-sketch <gunny0087@gmail.com>
2026-07-15 15:00:58 -0300

    Create numeronega.py

---
+++ numeronega.py
@@ -0,0 +1,19 @@
+soma = 0
+numeropositivo = 0
+
+while True:
+    numero = int(input("Digite um número: "))
+    if numero < 0:
+        break
+    soma += numero
+    if numeropositivo == 0:
+        maior = numero
+    else:
+        if numero > maior:
+            maior = numero
+    numeropositivo += 1
+if numeropositivo > 0:
+    media = soma/numeropositivo
+    print(f"A soma de todos os números é {soma}")
+    print(f"A média aitimética dos número é {media}")
+    print(f"O maior número é {maior}")
